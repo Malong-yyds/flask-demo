c599ad55386563fa22328b5ddb69e8630061c0b0
Malong-yyds <161089691@qq.com>
2024-03-01 19:36:47 +0800

    fix: 解决跨域问题

--- app/__init__.py
+++ app/__init__.py
@@ -1,20 +1,23 @@
 # coding:utf-8
 # demodemo\ihome\__init__.py
 from flask import Flask,jsonify
+from flask_cors import CORS
 from config import Config
 from extension import db
 
 def create_app():
     
     app = Flask(__name__)
+    CORS(app)
     app.config.from_object(Config)
-
+    app.config['SQLALCHEMY_REFLECT'] = True  # 启用表反射  
     # 使用app初始化db
     db.init_app(app)
 
     # 注册蓝图
-    from app.api import user
+    from app.api import user,attraction
     app.register_blueprint(user.user_bp, url_prefix="/api/user")
+    app.register_blueprint(attraction.attaction_bp, url_prefix="/api/attraction")
 
       
     return app
